cc616082fbf8c28c00bc2fc10b799eaa6746a84e
Josplanet <143292815+Josplanet@users.noreply.github.com>
2023-09-17 07:37:13 -0500

    logos sec 3

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -2,12 +2,12 @@ const logos = () => {
   return (
     <div className="w-full py-[56px] flex justify-center items-center">
       <div className="flex gap-[80px] items-center">
-        <img height={48} src="./logos/1.png" alt="" />
-        <img height={48} src="./logos/2.png" alt="" />
-        <img height={48} src="./logos/3.png" alt="" />
-        <img height={48} src="./logos/4.png" alt="" />
-        <img height={48} src="./logos/5.png" alt="" />
-        <img height={48} src="./logos/6.png" alt="" />
+        <img height={48} src="./Logos/1.png" alt="" />
+        <img height={48} src="./Logos/2.png" alt="" />
+        <img height={48} src="./Logos/3.png" alt="" />
+        <img height={48} src="./Logos/4.png" alt="" />
+        <img height={48} src="./Logos/5.png" alt="" />
+        <img height={48} src="./Logos/6.png" alt="" />
       </div>
     </div>
   );
